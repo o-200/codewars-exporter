0f155f5cafaebe40cbc85657ef5b77e01eb5ab6b
Alexander Abramov <omgout.200@gmail.com>
2024-02-13 19:27:40 +0700

    refactored file_saver

--- lib/codewars_exporter/parser/utils/file_saver.rb
+++ lib/codewars_exporter/parser/utils/file_saver.rb
@@ -1,17 +1,28 @@
 module Utils
-  module FileSaver
+  class FileSaver
     SOLUTION_FILE = 'solution.txt'
     LOGIN_URL = 'https://www.codewars.com/users/sign_in'
 
-    def start_save_solutions(choice, language)
+    def initialize(login, email, password, language, choice)
+      @login = login
+      @email = email
+      @password = password
+      @language = language
+      @choice = choice
+      @browser = Watir::Browser.new :firefox, headless: true
+
+      start_save_solutions
+    end
+
+    def start_save_solutions
       login
       puts 'Start parsing solutions!'
 
-      if choice == 1
-        parse.separate_data(language).place_by_files(language)
-        puts "#{solution_path(language)} was created! closing program..."
+      if @choice == 1
+        parse.separate_data.place_by_files
+        puts "#{solution_path} was created! closing program..."
       else
-        parse.separate_data(language).place_to_one_file
+        parse.separate_data.place_to_one_file
         puts "'#{SOLUTION_FILE}' was created! closing program..."
       end
     end
@@ -23,18 +34,19 @@ module Utils
       sleep(3)
 
       @browser.goto(LOGIN_URL)
-      @browser.text_field(id: 'user_email').set(email)
-      @browser.text_field(id: 'user_password').set(password)
+      @browser.text_field(id: 'user_email').set(@email)
+      @browser.text_field(id: 'user_password').set(@password)
       @browser.button(type: 'submit').click
 
+      puts 'logged'
       @browser
     end
 
     def parse
-      @browser.goto(solution_url(@nickname))
-      browser = scroll_to_bottom_page(@browser)
+      @browser.goto(solution_url)
+      scrolled_browser = scroll_to_bottom_page(@browser)
 
-      doc = Nokogiri::HTML.parse(browser.html)
+      doc = Nokogiri::HTML.parse(scrolled_browser.html)
       @data = doc.css('.list-item-solutions')
 
       puts 'parsing complete!'
@@ -45,8 +57,8 @@ module Utils
 
     # return array with hashes
     # hash-element is a solution which have name, kyu and solution
-    def separate_data(language)
-      @data = @data.select {|item| item.at('code')['data-language'] == language }
+    def separate_data
+      @data = @data.select {|item| item.at('code')['data-language'] == @language }
                   .map { |item|
         {
           solution_name: item.at_css('a').text,
@@ -60,13 +72,13 @@ module Utils
 
     # create many files-solutions
     # to separate folder
-    def place_by_files(language)
-      FileUtils.mkdir_p(solution_path(language))
+    def place_by_files
+      FileUtils.mkdir_p(solution_path)
 
       @data.each do |n|
         name_kyu = "#{n[:solution_name]} #{n[:kyu]}"
 
-        File.open(File.join(solution_path(language), name_kyu), 'w') do |file|
+        File.open(File.join(solution_path, name_kyu), 'w') do |file|
           file.write(n[:solution])
         end
 
@@ -82,8 +94,8 @@ module Utils
       end
     end
 
-    def solution_path(language)
-      "solutions/#{language}"
+    def solution_path
+      "solutions/#{@language}"
     end
 
     def scroll_to_bottom_page(browser)
@@ -94,6 +106,10 @@ module Utils
 
         return browser if browser.links.size == link_number
       end
+    end
+
+    def solution_url
+      "https://www.codewars.com/users/#{@login}/completed_solutions"
     end
   end
 end
