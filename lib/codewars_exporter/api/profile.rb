4b8d5eae93be3dd21ef38e22c1677dcc80cfab26
Alexander Abramov <omgout.200@gmail.com>
2024-02-14 19:10:56 +0700

    #json_request_parse spec

--- lib/codewars_exporter/api/profile.rb
+++ lib/codewars_exporter/api/profile.rb
@@ -6,8 +6,8 @@ require 'json'
 module Api
   class Profile
     def initialize(nickname, link = nil)
-      @request = generate_link(nickname, link)
-      @json = json_request_parse
+      request = generate_link(nickname, link)
+      @json = json_request_parse(request)
     end
 
     def full_data
@@ -44,13 +44,13 @@ module Api
       link ? link : "https://www.codewars.com/api/v1/users/#{nickname}"
     end
 
-    def json_request_parse
-      uri = URI(@request)
+    def json_request_parse(request)
+      uri = URI(request)
 
       response = begin
                    Net::HTTP.get(uri)
                  rescue Errno::ECONNREFUSED => e
-                   File.read(@request)
+                   File.read(request)
                  end
 
       JSON.parse(response)
--- spec/api/profile_spec.rb
+++ spec/api/profile_spec.rb
@@ -3,7 +3,7 @@ require './spec/spec_helper.rb'
 RSpec.describe Api::Profile do
   before do
     @test_file_path = './spec/test_files/api/profile/o-200.json'
-    @nick = '123'
+    @nick = 'o-200'
   end
 
   describe 'parse data' do
@@ -21,6 +21,13 @@ RSpec.describe Api::Profile do
     it 'dont sending link and should return constructed link' do
       test_class = Api::Profile.new(@nick)
       expect(test_class.send(:generate_link, @nick)).to eq("https://www.codewars.com/api/v1/users/#{@nick}")
+    end
+  end
+
+  describe '#json_request_parse' do
+    it 'should correct parse json' do
+      test_class = Api::Profile.new(@nick, @test_file_path)
+      expect(test_class.send(:json_request_parse, @test_file_path)).to eq(JSON.parse(File.read(@test_file_path)))
     end
   end
 end
