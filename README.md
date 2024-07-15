343572540c91135c504749333e0140452d6afbde
Alex Abramov <alexanderabramov24@gmail.com>
2024-07-15 20:10:56 +0700

    rspec

--- README.md
+++ README.md
@@ -35,10 +35,7 @@ https://dev.codewars.com/
 If you're rubyist you can check bin/* for any configs, but anyway use
 
 - use info from codewars api - `bin/api <email> <password>`
-- debugging - `bin/console`
 - parse solutions - `bin/parser <email> <password> <choice methods> <language>`
-
-P.S: choice methods and language parameters must be blank. (not anymore because docker)
 
 ### Choice methods
 
---
+++ lib/codewars_exporter/utils.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require './lib/codewars_exporter/utils/access_requester'
+require './lib/codewars_exporter/utils/constants'
+require './lib/codewars_exporter/utils/how_save_chooser'
+require './lib/codewars_exporter/utils/file_saver'
+require './lib/codewars_exporter/utils/nickname_parser'
+require './lib/codewars_exporter/utils/place_files/placer_to_one_file'
+require './lib/codewars_exporter/utils/place_files/placer_by_files'
+require './lib/codewars_exporter/utils/language_fetcher'
